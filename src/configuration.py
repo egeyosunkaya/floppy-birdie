6bd368c6261868ea47c56cd25779498f998c1287
ege <egeyosunkaya@gmail.com>
2020-11-23 17:29:08 +0300

    fix(configuration): Fixed debug mode always enabled bug Ege Yosunkaya

--- src/configuration.py
+++ src/configuration.py
@@ -1,10 +1,13 @@
 import os
 
-def __str_to_bool(val):
-    return str(val).capitalize() == "TRUE"
 
 class Configuration:
 
     @staticmethod
+    def __str_to_bool(val):
+        return str(val).capitalize() == "TRUE"
+
+
+    @staticmethod
     def is_debug_mode_enabled():
-        return os.environ.get("FLAPPY_DEBUG_ENABLED", "False")
+        return Configuration.__str_to_bool(os.environ.get("FLAPPY_DEBUG_ENABLED", "False"))
